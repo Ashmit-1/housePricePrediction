d9196a7ea1ab2cfd3cd063f9d16a8bb091184fcc
dev_das <dasashmit16@gmail.com>
2024-12-31 14:35:21 +0530

    modify ui to seperate input area in two columns

--- main.py
+++ main.py
@@ -16,10 +16,16 @@ st.title("House Price Prediction")
 longitude = st.slider("Enter your longitude", min_value=-150.0, max_value=-100.0, step=0.1, value=None)
 latitude = st.slider("Enter your latitude", min_value=30.0, max_value=50.0, step=0.1,value=None)
 house_age_slider = st.slider('Enter house age',value=None)
-population = st.number_input("Enter average population of the area", step=1, min_value=0, placeholder="Population...", value=None)
-households = st.number_input("Enter average number of households in the area", step=1, min_value=0, placeholder="Households...", value=None)
-income = st.number_input("Enter your income", step=1, min_value=0, placeholder="Income...", value=None)
-proximity_to_ocean = st.selectbox("Enter proximity to ocean", options=("<1H OCEAN", " INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"), index=None, placeholder="Proximity to ocean...")
+
+column1, column2 = st.columns(2)
+
+with column1:
+    population = st.number_input("Enter average population of the area", step=1, min_value=0, placeholder="Population...", value=None)
+    households = st.number_input("Enter average number of households in the area", step=1, min_value=0, placeholder="Households...", value=None)
+
+with column2:
+    income = st.number_input("Enter your income", step=1, min_value=0, placeholder="Income...", value=None)
+    proximity_to_ocean = st.selectbox("Enter proximity to ocean", options=("<1H OCEAN", " INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"), index=None, placeholder="Proximity to ocean...")
 
 data = {
     "longitude":[longitude],
