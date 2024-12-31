0b6f89b84e97b51b1ad94fae29d376b5f9bb0080
dev_das <dasashmit16@gmail.com>
2024-12-31 15:34:35 +0530

    containerize the input form

--- main.py
+++ main.py
@@ -14,32 +14,36 @@ total_rooms_median = df["total_rooms"].median()
 total_bed_rooms_median = df["total_bedrooms"].median()
 
 st.title("House Price Prediction", anchor=False)
-st.divider()
 
-st.markdown("##### Enter your longitude")
-longitude = st.slider("Enter your longitude", min_value=-150.0, max_value=-100.0, step=0.1, value=None, label_visibility="collapsed")
 
-st.markdown("##### Enter your latitude")
-latitude = st.slider("Enter your latitude", min_value=30.0, max_value=50.0, step=0.1,value=None, label_visibility="collapsed")
+inputForm = st.container(border=True)
 
-st.markdown("##### Enter house age")
-house_age_slider = st.slider('Enter house age',value=None, label_visibility="collapsed")
+with inputForm:
 
-column1, column2 = st.columns(2)
+    st.markdown("##### Enter your longitude")
+    longitude = st.slider("Enter your longitude", min_value=-150.0, max_value=-100.0, step=0.1, value=None, label_visibility="collapsed")
 
-with column1:
-    st.markdown("##### Enter average population of the area")
-    population = st.number_input("Enter average population of the area", step=1, min_value=0, placeholder="Population...", value=None, label_visibility="collapsed")
+    st.markdown("##### Enter your latitude")
+    latitude = st.slider("Enter your latitude", min_value=30.0, max_value=50.0, step=0.1,value=None, label_visibility="collapsed")
 
-    st.markdown("##### Enter average number of households")
-    households = st.number_input("Enter average number of households in the area", step=1, min_value=0, placeholder="Households...", value=None, label_visibility="collapsed")
+    st.markdown("##### Enter house age")
+    house_age_slider = st.slider('Enter house age',value=None, label_visibility="collapsed")
 
-with column2:
-    st.markdown("##### Enter your income")
-    income = st.number_input("Enter your income", step=1, min_value=0, placeholder="Income...", value=None, label_visibility="collapsed")
+    column1, column2 = st.columns(2)
 
-    st.markdown("##### Enter proximity to ocean")
-    proximity_to_ocean = st.selectbox("Enter proximity to ocean", options=("<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"), index=None, placeholder="Proximity to ocean...", label_visibility="collapsed")
+    with column1:
+        st.markdown("##### Enter average population of the area")
+        population = st.number_input("Enter average population of the area", step=1, min_value=0, placeholder="Population...", value=None, label_visibility="collapsed")
+
+        st.markdown("##### Enter your income")
+        income = st.number_input("Enter your income", step=1, min_value=0, placeholder="Income...", value=None, label_visibility="collapsed")
+
+    with column2:
+        st.markdown("##### Enter average number of households")
+        households = st.number_input("Enter average number of households in the area", step=1, min_value=0, placeholder="Households...", value=None, label_visibility="collapsed")
+
+        st.markdown("##### Enter proximity to ocean")
+        proximity_to_ocean = st.selectbox("Enter proximity to ocean", options=("<1H OCEAN", "INLAND", "NEAR OCEAN", "NEAR BAY", "ISLAND"), index=None, placeholder="Proximity to ocean...", label_visibility="collapsed")
 
 data = {
     "longitude":[longitude],
@@ -79,7 +83,7 @@ if predict:
         prepared_data = new_full_pipeline.transform(user_data)
         modelPrediction = myModel.predict(prepared_data)
         for i in modelPrediction:
-            st.subheader(f"The predicted house price is ${i:.2f}", anchor=False)
+            st.subheader(f"The predicted house price is $ {i:.2f}", anchor=False)
             break
     else:
         st.warning("Oops you missed some information !")
