d359d2fde848cd07455941e816c0588136948016
dev_das <dasashmit16@gmail.com>
2025-02-15 14:39:04 +0530

    handle form inputs in flask app

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
  
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+    elif request.method == 'POST':
+        return render_template("greet.html", latitude=request.form.get("latitude"), longitude=request.form .get("longitude"), households=request.form.get("households"), age=request.form.get("age"), population=request.form.get("population"), proximity=request.form.get("proximity"), income=request.form.get("income"))
+        # return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run()
